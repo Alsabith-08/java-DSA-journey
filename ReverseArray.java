05fa5573f50b32690dd1898ee374d643442111ee
Alsabith <alsabith080207@gmail.com>
2026-07-03 18:07:03 +0530

    Enhance output with before and after array states
    Added print statements to show array before and after reversal.

--- ReverseArray.java
+++ ReverseArray.java
@@ -1,21 +1,20 @@
-package ChatGPT_Problems;
+
 // just swap the two pointer values that all for the reverse the arr
 // i from start and  j from last index just swap and increment the i value and decrement the j value
 // the condition ( i < j ) break the array is reversed.
 public class ReverseArray {
     public static void main(String[] args) {
         int[] arr ={1,2,3,4,7,8};
-
+        //before sorted
         for(int nums : arr) {
             System.out.print(nums + " ");
         }
         System.out.println();
 
         reverse(arr);
-
-        for(int num : arr){
-            System.out.print(num + " ");
-        }
+        //After sorted
+        System.out.println(Arrays.toString(arr));
+        
     }
     static void  reverse(int[] arr){
         int i=0 , j =  arr.length-1;
