4f4059c6f6a4826d45629215c3bbf36c73c57e1b
Alsabith <alsabith080207@gmail.com>
2026-07-10 17:31:59 +0530

    Refactor PalindromeString for case insensitivity
    Updated the palindrome check to handle case sensitivity and added comments for clarity.

--- Strings/PalindromeString.java
+++ Strings/PalindromeString.java
@@ -4,23 +4,24 @@ import java.util.Scanner;
 
 public class PalindromeString {
     public static void main(String[] args) {
-      String s = "A man, a plan, a canal: Panama";
-
-        System.out.println(isPalindrome(s));
+        
+     String s = "Madam";
+     System.out.println(isPalindrome(s));
 
     }
     static boolean isPalindrome(String s){
 
-        s.toLowerCase().replaceAll("[A-Za-z0-9]" , "");
-        int i =0;
+        s = s.toLowerCase().replaceAll("[A-Za-z0-9]" , "");  // using Regex Expression for remove the unwanted values 
+         // take two pointers
+        int i =0;    
         int j =s.length() -1;
 
         while(i <= j){
-            if(s.charAt(i) != s.charAt(j)){
+            if(s.charAt(i) != s.charAt(j)){  // use the charAt() method to check the first and last values are same if not same return false
                 return false;
             }
-            i++;
-            j--;
+            i++;  // increment starting pointer
+            j--;  // decrement last pointer
         }
         return true;
     }
