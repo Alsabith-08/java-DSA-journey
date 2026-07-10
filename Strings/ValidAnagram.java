7b6ddcd1fc3c2e3a66c26950c59acb8a39b4ad74
Alsabith <alsabith080207@gmail.com>
2026-07-10 17:53:41 +0530

    Add comments to ValidAnagram class for clarity
    Added comments to clarify the logic and functionality of the checkAnagram method.

--- Strings/ValidAnagram.java
+++ Strings/ValidAnagram.java
@@ -10,24 +10,28 @@ public class ValidAnagram {
     }
     static boolean checkAnagram(String str1 , String str2){
 
-        str1 = str1.toLowerCase();
+        str1 = str1.toLowerCase();  // change the strings into lowerCase using toLowerCase() method
         str2 = str2.toLowerCase();
 
-        int m = str1.length();
+        int m = str1.length();  // in string to find length must put the brackets in last() 
         int n = str2.length();
 
-        if(m != n){
+        if(m != n){  // the 2 strings length not equal return false 
             return false;
         }else {
-            int[] count  = new int[26];
+            int[] count  = new int[26];  // create a count[] array for all alphabets
 
-            for (int i = 0; i < m; i++) {
+            for (int i = 0; i < m; i++) {  // put 1 on array based on the first string value is  "Deco"
                 count[str1.charAt(i) - 'a' ] ++;
             }
-            for (int i = 0; i < n ; i++) {
+            for (int i = 0; i < n ; i++) {  // remove all 1 on the same array based on the second string value is "Dcoe"
                 count[str2.charAt(i) - 'a'] --;
             }
-            for (int i = 0; i < count.length; i++) {
+
+            // so all array is 0 
+            // if any value is not a 0
+            //return false
+            for (int i = 0; i < count.length; i++) {  
                 if(count[i] != 0){
                     return false;
                 }
