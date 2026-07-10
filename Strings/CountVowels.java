5ea7bc3b7377479278b3b1193fb7d9d44099bcbb
Alsabith <alsabith080207@gmail.com>
2026-07-10 17:39:21 +0530

    Enhance CountVowels.java with explanatory comments
    Added comments to explain the logic for counting vowels.

--- Strings/CountVowels.java
+++ Strings/CountVowels.java
@@ -7,9 +7,14 @@ public class CountVowels {
     }
     static int countVowels(String s){
 
-        int count = 0;
+        int count = 0;  // declare the count value as a 0
+        // using the for loop to traverse the String 
         for(int i= 0; i< s.length() ; i++){
-            char c = s.charAt(i);
+            // we are going to check  charactr by character(letter by letter) , so using the charAt() method
+            char c = s.charAt(i);  
+            // check the character is vowels or not 
+            // if yes increment the count value 
+            // then return count
             if(c =='a'||c =='e'||c =='i'||c =='o'||c =='u'||
                     c =='A'||c =='E'||c =='I'||c =='O'||c =='U')
             {
