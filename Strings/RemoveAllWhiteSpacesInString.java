38c63dde146d74fe00581103e5350aa5b8f1fe7c
Alsabith <alsabith080207@gmail.com>
2026-07-11 22:41:37 +0530

    Add files via upload

---
+++ Strings/RemoveAllWhiteSpacesInString.java
@@ -0,0 +1,16 @@
+package ChatGPT_Problems.Strings;
+
+public class RemoveAllWhiteSpacesInString {
+    public static void main(String[] args) {
+        String str1 = "    Al sabith berlin     ";
+
+        String str2 = str1.trim();   //remove the white spaces from the starting and last of the string
+
+        String str3 = str1.replaceAll("\\s" , "");  // this is the unicode , \s -> means single space so the single space replace by the nothing
+        System.out.println(str1);  // original String
+
+        System.out.println(str2);
+        System.out.println(str3);
+
+    }
+}
