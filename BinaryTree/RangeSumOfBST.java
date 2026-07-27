4e8845c7e620d17b69e999883348a993f93fbb96
Alsabith <alsabith080207@gmail.com>
2026-07-27 18:13:54 +0530

    Add files via upload

---
+++ BinaryTree/RangeSumOfBST.java
@@ -0,0 +1,64 @@
+package ChatGPT_Problems.BinarySearchTree;
+
+public class RangeSumOfBST {
+    public static void main(String[] args) {
+        BST tree = new BST();
+        TreeNode root = null;
+
+        root = tree.insert(root , 12);
+        root = tree.insert(root , 54);
+        root = tree.insert(root , 17);
+        root = tree.insert(root , 60);
+        root = tree.insert(root , 12);
+        root = tree.insert(root , 88);
+        root = tree.insert(root , 10);
+        root = tree.insert(root , 18);
+        root = tree.insert(root , 30);
+        root = tree.insert(root , 40);
+
+        System.out.println(tree.rangeSum(root , 50 , 70));
+
+    }
+    static class TreeNode{
+        int data;
+        TreeNode left , right;
+
+        public TreeNode(int data){
+            this.data = data;
+        }
+    }
+    static class BST{
+        TreeNode insert(TreeNode root , int data){
+            if(root == null){
+                return new TreeNode(data);
+            }
+
+            if(data < root.data){
+                root.left = insert(root.left , data);
+            }else{
+                root.right = insert(root.right , data);
+            }
+            return root;
+        }
+        public int rangeSum(TreeNode root , int low , int high){
+            if(root == null){
+                return 0;
+            }
+
+            int sum = 0;
+            if(root.data > low){
+                sum += rangeSum(root.left , low , high);
+            }
+
+            if(root.data >= low && root.data <= high){
+                sum += root.data;
+            }
+
+            if(root.data < high){
+                sum += rangeSum(root.right , low ,high);
+            }
+            return sum;
+        }
+    }
+
+}
