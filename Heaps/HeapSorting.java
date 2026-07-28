9af0d5e6325395c1358535af128cd026d3362eed
Alsabith <alsabith080207@gmail.com>
2026-07-28 19:11:41 +0530

    Enhance HeapSorting.java with explanatory comments
    Added comments to explain heap sort process.

--- Heaps/HeapSorting.java
+++ Heaps/HeapSorting.java
@@ -1,9 +1,19 @@
-package ChatGPT_Problems.Heap;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
-
+// flow 
+/* Normal Array
+       |
+   Build Max Heap
+       |
+   Swap Root <-> Last
+       |
+   Reduce Heap Size
+       |
+   Heapify Dowm
+       |
+   Repeat Until one element remains
+       |
+   Sorted Array
+*/
 public class HeapSorting {
     public static void main(String[] args) {
 
@@ -18,29 +28,35 @@ public class HeapSorting {
 
             while(true) {
                 int largest = index;
+                // left child
                 int left = 2 * index + 1;
+                // right child
                 int right = 2 * index + 2;
-
+                // compare left child
                 if(left < size && arr[left] > arr[largest]){
                     largest = left;
                 }
-
+                // compare right child
                 if(right < size && arr[right] > arr[largest]){
                     largest = right;
                 }
-
+                // heap Already correct
+                // if the parent is still the largest
+                // no swap is needed
                 if(largest == index){
                     break;
                 }
-
+                // Normal swap
                 int temp = arr[index];
                 arr[index] = arr[largest];
                 arr[largest] = temp;
-
+                // continue Downward
+                // Move to the child
+                // continue fixing
                 index = largest;
             }
         }
-
+        // convert a normal array into Max Heap
         static void buildHeap(int[] arr){
             int n = arr.length;
 
@@ -48,16 +64,21 @@ public class HeapSorting {
                 heapifyDown(arr , n ,i);
             }
         }
-
+        // Main sorting function 
         public static void heapSort(int[] arr){
+            
             int n = arr.length;
             buildHeap(arr);
-
+            //Each time, the root(largest element) 
+            // moves to the end
             for(int i = n-1 ; i >=0 ; i--){
+                // save root
                 int temp = arr[0];
+                // Move Last element to Root
                 arr[0] = arr[i];
+                // put largest at the end
                 arr[i] = temp;
-
+                // fix Remaining Heap
                 heapifyDown(arr , i, 0);
             }
         }
