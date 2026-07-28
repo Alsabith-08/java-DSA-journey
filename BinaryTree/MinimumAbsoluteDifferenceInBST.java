ba3e41e5ad34514d8668c2216175de885effd821
Alsabith <alsabith080207@gmail.com>
2026-07-28 23:24:05 +0530

    Add files via upload

---
+++ BinaryTree/MinimumAbsoluteDifferenceInBST.java
@@ -0,0 +1,64 @@
+package ChatGPT_Problems.BinarySearchTree;
+
+public class MinimumAbsoluteDifferenceInBST {
+    public static void main(String[] args) {
+
+        BST tree = new BST();
+        TreeNode  root = null;
+
+        root = tree.insert(root ,4 );
+        root = tree.insert(root , 2);
+        root = tree.insert(root , 6);
+        root = tree.insert(root , 1);
+        root = tree.insert(root , 3);
+
+        System.out.println(tree.getMinDifference(root));
+    }
+    static class TreeNode{
+        int data;
+        TreeNode left , right;
+
+        public TreeNode(int data){
+            this.data = data;
+        }
+    }
+    static class BST {
+        TreeNode insert(TreeNode root, int data) {
+
+            if (root == null) {
+                return new TreeNode(data);
+            }
+
+            if (data < root.data) {
+                root.left = insert(root.left, data);
+            } else {
+                root.right = insert(root.right, data);
+            }
+            return root;
+        }
+
+        public static  int getMinDifference(TreeNode root){
+            int minDifference = Integer.MAX_VALUE;
+            Integer prev = null;
+            // base Condition
+            if(root == null){
+                return minDifference;
+            }
+
+            getMinDifference(root.left);
+
+            // process Root
+            if(prev != null){
+                minDifference = Math.min(minDifference , root.data-prev);
+            }
+
+            if(root != null){
+                prev = root.data;
+            }
+
+            getMinDifference(root.right);
+
+            return minDifference;
+        }
+    }
+}
