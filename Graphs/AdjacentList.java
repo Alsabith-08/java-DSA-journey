bd8fa9be41db0a000bb625577e7cba176ae99a0a
Alsabith <alsabith080207@gmail.com>
2026-07-29 19:21:48 +0530

    Refactor AdjacentList.java with better comments
    Updated comments for clarity and improved formatting.

--- Graphs/AdjacentList.java
+++ Graphs/AdjacentList.java
@@ -1,16 +1,17 @@
-package ChatGPT_Problems.Graphs;
-
 import java.util.ArrayList;
 
+// using the ArrayList for representation of graph
+// it's better than the Adjacent matrix
 public class AdjacentList {
     public static void main(String[] args) {
         // List
         int vertices = 4;
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-
+    
         for (int i = 0; i < vertices; i++) {
             graph.add(new ArrayList<>());
         }
+        
         graph.get(0).add(1);
         graph.get(0).add(2);
 
@@ -23,9 +24,10 @@ public class AdjacentList {
 
         graph.get(3).add(2);
 
+        // outer loop for print the vertices
         for (int i = 0; i < vertices; i++) {
             System.out.print(i + " - ");
-
+            // inner loop for print the each vertices of edges
             for(int neighbour : graph.get(i)){
                 System.out.print(neighbour + " ");
             }
