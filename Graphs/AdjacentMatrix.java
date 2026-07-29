2d5346c616495ad4f1b6047216eb59c70e27287a
Alsabith <alsabith080207@gmail.com>
2026-07-29 19:23:10 +0530

    Enhance comments in AdjacentMatrix class
    Added comments to clarify graph representation.

--- Graphs/AdjacentMatrix.java
+++ Graphs/AdjacentMatrix.java
@@ -1,8 +1,9 @@
-package ChatGPT_Problems.Graphs;
-
 public class AdjacentMatrix {
     public static void main(String[] args) {
         // graphs representation in matrix
+        // 0 - means not connected
+        // 1 - means connected
+        // row , column - vertices
         int [][] graph = {
                 {0,1,1,0},
                 {1,0,1,0},
@@ -10,7 +11,7 @@ public class AdjacentMatrix {
                 {0,0,1,0}
         };
 
-        System.out.println(graph[2][1]);
-        System.out.println(graph[3][1]);
+        System.out.println(graph[2][1]);  // 1
+        System.out.println(graph[3][1]);  // 0
     }
 }
