28dad0e1f8c12161c898dc9c2ce8cb5c162edcf2
Alsabith <alsabith080207@gmail.com>
2026-08-06 22:50:25 +0530

    Add files via upload

---
+++ Queue/KthLargestElement.java
@@ -0,0 +1,23 @@
+package ChatGPT_Problems.Heap;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class KthLargestElement {
+    public static void main(String[] args) {
+        int[] arr ={1,4,6,7,8,9};
+        int k = 4;
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int num : arr){
+            queue.add(num);
+        }
+
+        for(int i = 1 ; i < k ; i++){
+            queue.poll();
+        }
+
+        System.out.println(k + " Largest element " + queue.peek());
+    }
+}
