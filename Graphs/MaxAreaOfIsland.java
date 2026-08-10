223f2d27918fb0cd7abf20edb55cc7c66bc99b97
Alsabith <alsabith080207@gmail.com>
2026-08-10 23:19:38 +0530

    Add files via upload

---
+++ Graphs/MaxAreaOfIsland.java
@@ -0,0 +1,48 @@
+package ChatGPT_Problems.Graphs;
+
+public class MaxAreaOfIsland {
+    public static void main(String[] args) {
+        int[][] grid = {
+                {1,1,0,0},
+                {1,1,0,0},
+                {0,1,1,1},
+                {1,0,0,0}
+        };
+        System.out.println(MaxAreaOfIsland(grid));
+    }
+    static int MaxAreaOfIsland(int[][] grid){
+
+        int maxArea = 0;
+
+        for (int i = 0; i <grid.length ; i++) {
+            for (int j = 0; j <grid[0].length ; j++) {
+
+                //found an island
+                if(grid[i][j] == 1){
+                    int area = dfs(grid , i, j);
+
+                    maxArea = Math.max(area , maxArea);
+                }
+            }
+        }
+        return maxArea;
+    }
+    static int dfs(int[][] grid , int row , int col){
+        if(row < 0 || col <0 || row >= grid.length || col >= grid[0].length || grid[row][col] == 0){
+            return 0;
+        }
+
+        grid[row][col] = 0;
+
+        int up = dfs(grid , row- 1 , col);
+
+        int down = dfs(grid , row+1 , col);
+
+        int left = dfs(grid , row , col-1);
+
+        int right = dfs(grid , row , col+1);
+
+        return 1+ up + down + left+ right;
+    }
+
+}
---
+++ Graphs/NumberOfProvinces.java
@@ -0,0 +1,38 @@
+package ChatGPT_Problems.Graphs;
+
+public class NumberOfProvinces {
+    public static void main(String[] args) {
+        int[][] isConnected = {
+                {1,1,0},
+                {1,1,0},
+                {1,0,1},
+        };
+        System.out.println(findnum(isConnected));
+    }
+    static void dfs(int city , int[][] isConnected , boolean[] visited){
+
+        visited[city] = true;
+
+        for (int nextCity = 0; nextCity < isConnected.length ; nextCity++) {
+            if(isConnected[city][nextCity] == 1 && !visited[nextCity]){
+                dfs(nextCity , isConnected , visited);
+            }
+        }
+    }
+
+    static int findnum(int[][] isConnected){
+
+        int n = isConnected.length;
+        boolean[] visited = new boolean[n];
+
+        int provinces = 0;
+
+        for (int i = 0; i <n ; i++) {
+            if(!visited[i]){
+                provinces++;
+                dfs( i, isConnected , visited);
+            }
+        }
+        return provinces;
+    }
+}
