5e074811a0db624b1f11ee569be6970f76cd886d
Alsabith <alsabith080207@gmail.com>
2026-08-10 23:32:48 +0530

    Add files via upload

---
+++ Graphs/KeyAndRooms.java
@@ -0,0 +1,42 @@
+package ChatGPT_Problems.Graphs;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class KeyAndRooms {
+    public static void main(String[] args) {
+
+        List<List<Integer>> rooms = new ArrayList<>();
+
+        rooms.add(Arrays.asList(1));
+        rooms.add(Arrays.asList(2));
+        rooms.add(Arrays.asList(3));
+        rooms.add(Arrays.asList());
+
+        System.out.println(canVisitAllRooms(rooms));
+
+    }
+    static void dfs(int room , List<List<Integer>> rooms , boolean[] visited){
+        visited[room] = true;
+        System.out.println();
+
+        for(int key : rooms.get(room)){
+            if(!visited[key]){
+                dfs(key , rooms, visited);
+            }
+        }
+    }
+    static boolean canVisitAllRooms(List<List<Integer>> rooms){
+        boolean[] visited = new boolean[rooms.size()];
+
+        dfs(0 , rooms , visited);
+
+        for(boolean room : visited){
+            if(!room){
+                return false;
+            }
+        }
+        return true;
+    }
+}
