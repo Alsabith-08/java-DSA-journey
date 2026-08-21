42ce6a59622b544b2f52ea792ded86c3af826b93
Alsabith <alsabith080207@gmail.com>
2026-08-21 23:02:32 +0530

    Refactor MergeStringsAlternately_1768.java formatting

--- Strings/MergeStringsAlternately_1768.java
+++ Strings/MergeStringsAlternately_1768.java
@@ -2,6 +2,14 @@
 
 // Merge the String letter by letter
 
+/*
+   1. String.charAt(index) 
+   2. String.length()
+   3. StringBuilder
+   4. TwoPointers
+   5. Boundary Checking
+*/
+
 public class MergeStringsAlternately_1768 {
     public static void main(String[] args) {
         String word1 = "alsa";
