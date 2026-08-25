5b48ed63ef81019e590106f22cc8acc3ba5cd9c2
Alsabith <alsabith080207@gmail.com>
2026-08-25 20:41:51 +0530

    Add files via upload

---
+++ Array/MissingNumber_268.java
@@ -0,0 +1,40 @@
+package LeetCodeEx.Strings;
+
+import java.util.Arrays;
+
+public class MissingNumber_268 {
+    public static void main(String[] args) {
+        int[] nums = {2,3,0,1};
+
+        System.out.println("Array :" + Arrays.toString(nums));
+
+        int result = missingNum(nums);
+        System.out.println("Missing Number :" + result);
+
+        int result1 = missingNumber(nums);
+        System.out.println("Missing Number :"+result1);
+    }
+
+    // XOR Rules - O(n)
+    static int missingNum(int[] nums){
+        // initialize xor
+        int xor = nums.length;
+
+        for (int i = 0; i <nums.length ; i++) {
+            xor = xor ^ i ^ nums[i];
+        }
+        return xor;
+    }
+
+    // using sort - O(n log n)
+    static int missingNumber(int[] nums){
+        Arrays.sort(nums);
+
+        for (int i = 0; i <nums.length ; i++) {
+            if(nums[i] != i){
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
