ccaba80147c9afd22fa309f180c6679040ece032
Alsabith <alsabith080207@gmail.com>
2026-08-26 21:37:08 +0530

    Add files via upload

---
+++ Array/ProductOfArrayExceptSelf_238.java
@@ -0,0 +1,36 @@
+package LeetCodeEx.Arrays;
+
+import java.util.Arrays;
+
+public class ProductOfArrayExceptSelf_238 {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4};
+
+        int[] result = productExcept(nums);
+
+        System.out.println("Input :" + Arrays.toString(nums));
+        System.out.println("Output : "+ Arrays.toString(result));
+    }
+    static int[] productExcept(int[] nums){
+        int n = nums.length;
+
+        // answer[i] = leftProduct * rightProduct
+        int[] answer  =new int[n];
+
+        // store prefix products - contains product of everything before the current index
+        int prefix = 1;
+        for (int i = 0; i < n; i++) {
+            answer[i] = prefix;
+            prefix = prefix * nums[i];
+        }
+
+        // Multiply by suffix products - contains product of everything after the current index
+        int sufix = 1;
+        for (int i = n-1; i >=0 ; i--) {
+            answer[i] = answer[i] * sufix;
+            sufix = sufix*nums[i];
+        }
+
+        return answer;
+    }
+}
