74d80697e20aecd4b31f8fe787d26f4d0666bc5a
Alsabith <alsabith080207@gmail.com>
2026-09-15 18:45:22 +0530

    Update RemovingStar_2390.java

--- StackAndQueue_Problems/RemovingStar_2390.java
+++ StackAndQueue_Problems/RemovingStar_2390.java
@@ -1,22 +1,8 @@
-package LeetCodeEx.StackAndQueue;
 
 // https://leetcode.com/problems/removing-stars-from-a-string/description/
 // Approach : Stack(Last in First Out)
 // Time Complexity : O(n)
 
-/*              problem Description :
-  You are given a string s, which contains stars *.
-
-  In one operation, you can:
-
-  Choose a star in s.
-  Remove the closest non-star character to its left, as well as remove the star itself.
-  Return the string after all stars have been removed.
-
-  Note:
-  The input will be generated such that the operation is always possible.
-  It can be shown that the resulting string will always be unique.
-  */
 public class RemovingStar_2390 {
     public static void main(String[] args) {
         String s = "leet**cod*e";
@@ -25,15 +11,20 @@ public class RemovingStar_2390 {
     }
     static String removeStars(String s){
 
+      // use StringBuilder because it is mutable and the answer keeps changing while processing the String
+      // StringBuilder acts as a stack 
         StringBuilder stack = new StringBuilder();
+      
         for(char ch : s.toCharArray()){
 
+          // if character is *(Star) is found remove the previous element , otherwise add to Stack
             if(ch == '*'){
                 stack.deleteCharAt(stack.length() - 1);
             }else{
                 stack.append(ch);
             }
         }
+      // return the final string
         return stack.toString();
     }
 }
