d2f6d49275e69022ac58ca1b3782e09cbb2d0e61
Alsabith <alsabith080207@gmail.com>
2026-09-16 22:22:35 +0530

    Refactor NumberOfRecentCalls_933.java
    Removed unnecessary comments and adjusted formatting.

--- StackAndQueue_Problems/NumberOfRecentCalls_933.java
+++ StackAndQueue_Problems/NumberOfRecentCalls_933.java
@@ -1,16 +1,6 @@
-package LeetCodeEx.StackAndQueue;
-
 // https://leetcode.com/problems/number-of-recent-calls/description/
-
-/* You have a RecentCounter class which counts the number of recent requests within a certain time frame.
-
-   Implement the RecentCounter class:
-
-   RecentCounter() Initializes the counter with zero recent requests.
-   int ping(int t) Adds a new request at time t, where t represents some time in milliseconds,
-   and returns the number of requests that have happened in the inclusive range [t - 3000, t], that is, the new request plus every earlier request that is no more than 3000 milliseconds older.
-   It is guaranteed that every call to ping uses a strictly larger value of t than the previous call.
-   */
+// Apporach : Queue(First in First Out)
+// Time Complexity : O(n)
 
 import java.util.LinkedList;
 import java.util.Queue;
