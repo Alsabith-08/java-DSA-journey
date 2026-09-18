bc3d293b6e49275ebf4509bdb1b798e7df9d4da2
Alsabith <alsabith080207@gmail.com>
2026-09-18 18:27:25 +0530

    Refactor MaxTwinSum_2130 with improved comments
    Updated comments for clarity and added complexity analysis.

--- LinkedList_Problems/MaxTwinSum_2130.java
+++ LinkedList_Problems/MaxTwinSum_2130.java
@@ -1,17 +1,7 @@
-package LeetCodeEx.LinkedList;
 
 // https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list/description/
-
-/*
-   In a linked list of size n, where n is even, the ith node (0-indexed) of the linked list is known
-   as the twin of the (n-1-i)th node, if 0 <= i <= (n / 2) - 1.
-
-   For example, if n = 4, then node 0 is the twin of node 3, and node 1 is the twin of node 2.
-   These are the only nodes with twins for n = 4.
-   The twin sum is defined as the sum of a node and its twin.
-
-   Given the head of a linked list with even length, return the maximum twin sum of the linked list.
-*/
+// Approach : slipt into half , reverse the second half then add linearly to get the max sum
+// Time Complexity : O(n) , Space Complexity : O(1)
 public class MaxTwinSum_2130 {
     public static void main(String[] args) {
 
