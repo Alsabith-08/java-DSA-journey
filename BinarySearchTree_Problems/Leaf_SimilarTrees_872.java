e043c26e3635ac0f6563f963a310c7864a949cce
Alsabith <alsabith080207@gmail.com>
2026-09-19 19:44:55 +0530

    Update Leaf_SimilarTrees_872.java

--- BinarySearchTree_Problems/Leaf_SimilarTrees_872.java
+++ BinarySearchTree_Problems/Leaf_SimilarTrees_872.java
@@ -1,19 +1,8 @@
-package LeetCodeEx.BST;
-
 
 // https://leetcode.com/problems/leaf-similar-trees/description/
 // Approach : DFS to store the leaf node by order then compare
 // Time Complexity : O(n + m) , Space Complexity : O(n + m)
 
-/*   Consider all the leaves of a binary tree, from left to right order,
-     the values of those leaves form a leaf value sequence.
-
-     For example, in the given tree above, the leaf value sequence is (6, 7, 4, 9, 8).
-
-    Two binary trees are considered leaf-similar if their leaf value sequence is the same.
-
-    Return true if and only if the two given trees with head nodes root1 and root2 are leaf-similar.
-    */
 import java.util.ArrayList;
 import java.util.List;
 
