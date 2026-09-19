44f149466df4bdcbe6267f2db0b4e853b6c47173
Alsabith <alsabith080207@gmail.com>
2026-09-19 19:44:30 +0530

    Add files via upload

---
+++ BinarySearchTree_Problems/Leaf_SimilarTrees_872.java
@@ -0,0 +1,99 @@
+package LeetCodeEx.BST;
+
+
+// https://leetcode.com/problems/leaf-similar-trees/description/
+// Approach : DFS to store the leaf node by order then compare
+// Time Complexity : O(n + m) , Space Complexity : O(n + m)
+
+/*   Consider all the leaves of a binary tree, from left to right order,
+     the values of those leaves form a leaf value sequence.
+
+     For example, in the given tree above, the leaf value sequence is (6, 7, 4, 9, 8).
+
+    Two binary trees are considered leaf-similar if their leaf value sequence is the same.
+
+    Return true if and only if the two given trees with head nodes root1 and root2 are leaf-similar.
+    */
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leaf_SimilarTrees_872 {
+    public static void main(String[] args) {
+
+        TreeNode root1 = null;
+        root1 = createTree1(root1 , 33);
+        root1 = createTree1(root1 , 21);
+        root1 = createTree1(root1 , 43);
+        root1 = createTree1(root1 , 12);
+        root1 = createTree1(root1 , 65);
+
+        TreeNode root2 = null;
+        root2 = createTree2(root2 , 33);
+        root2 = createTree1(root2 , 21);
+        root2 = createTree1(root2 , 43);
+        root2 = createTree1(root2 , 12);
+        root2 = createTree1(root2 , 65);
+
+        System.out.println(similarLeaf(root1 , root2));
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    static TreeNode createTree1(TreeNode root, int val){
+        if(root == null){
+            return new TreeNode(val);
+        }
+
+        if(val < root.val){
+            root.left = createTree1(root.left , val);
+        }else{
+            root.right = createTree1(root.right , val);
+        }
+        return root;
+    }
+    static TreeNode createTree2(TreeNode root , int val){
+        if(root == null){
+            return new TreeNode(val);
+        }
+
+        if(val < root.val){
+            root.left = createTree2(root.left , val);
+        }else{
+            root.right = createTree2(root.right, val);
+        }
+        return root;
+    }
+
+    static void getLeaves(TreeNode root , List<Integer> leaves){
+        if(root == null){
+            return;
+        }
+        if(root.left == null && root.right == null){
+            leaves.add(root.val);
+            return;
+        }
+
+        getLeaves(root.left , leaves);
+        getLeaves(root.right , leaves);
+    }
+
+    static boolean similarLeaf(TreeNode root1 , TreeNode root2){
+
+        List<Integer> leaves1 = new ArrayList<>();
+        List<Integer> leaves2 = new ArrayList<>();
+
+        getLeaves(root1 , leaves1);
+        getLeaves(root2 , leaves2);
+
+        return leaves1.equals(leaves2);
+    }
+
+}
---
+++ BinarySearchTree_Problems/MaxDepthOfBT_104.java
@@ -0,0 +1,71 @@
+package LeetCodeEx.BST;
+
+
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+/*  Given the root of a binary tree, return its maximum depth.
+
+   A binary tree's maximum depth is the number of nodes along the
+   longest path from the root node down to the farthest leaf node.
+*/
+// Approach - Recursion
+// Time Complexity - O(n)
+// Space Complexity - O(log n) - Best Case(Balanced Tree)
+//                  - O(n) - Worst Case (Skewed Tree)
+
+// Maximum Depth = no.of.nodes on the longest path from thr root to a leaf
+// Maximum Depth (nodes) - 4
+// Longest Path (edges) - 3
+
+public class MaxDepthOfBT_104 {
+    public static void main(String[] args) {
+
+        BinaryTree tree = new BinaryTree();
+        TreeNode root = null;
+
+        root = tree.insert(root , 50);
+        root = tree.insert(root , 20);
+        root = tree.insert(root , 55);
+        root = tree.insert(root , 40);
+        root = tree.insert(root , 60);
+        root = tree.insert(root , 75);
+        root = tree.insert(root , 30);
+
+        System.out.println(tree.maxDepth(root));
+
+    }
+    static class TreeNode{
+        int data;
+        TreeNode left,right;
+
+        public TreeNode(int data){
+            this.data = data;
+        }
+    }
+    static class BinaryTree{
+        TreeNode insert(TreeNode root , int data){
+            if(root == null){
+                return new TreeNode(data);
+            }
+
+            if(data < root.data){
+                root.left = insert(root.left , data);
+            }else{
+                root.right = insert(root.right , data);
+            }
+            return root;
+        }
+
+      public int maxDepth(TreeNode root){
+            if(root == null){
+                return 0;
+            }
+
+            int leftDepth = maxDepth(root.left);
+
+            int rightDepth = maxDepth(root.right);
+
+            return 1+Math.max(leftDepth , rightDepth);
+        }
+    }
+
+}
