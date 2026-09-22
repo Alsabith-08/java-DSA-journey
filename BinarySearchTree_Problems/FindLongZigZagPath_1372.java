fe11c71f037aec5daeeda6a20932a3099152500b
Alsabith <alsabith080207@gmail.com>
2026-09-22 19:31:53 +0530

    Refactor FindLongZigZagPath_1372.java with comments
    Refactor code for clarity and maintainability. Added comments explaining approach and complexity.

--- BinarySearchTree_Problems/FindLongZigZagPath_1372.java
+++ BinarySearchTree_Problems/FindLongZigZagPath_1372.java
@@ -1,19 +1,7 @@
-package LeetCodeEx.BST;
 
 // https://leetcode.com/problems/longest-zigzag-path-in-a-binary-tree/description/
-
-/*   You are given the root of a binary tree.
-
-    A ZigZag path for a binary tree is defined as follow:
-
-    Choose any node in the binary tree and a direction (right or left).
-    If the current direction is right, move to the right child of the current node; otherwise, move to the left child.
-    Change the direction from right to left or from left to right.
-    Repeat the second and third steps until you can't move in the tree.
-    Zigzag length is defined as the number of nodes visited - 1. (A single node has a length of 0).
-
-    Return the longest ZigZag path contained in that tree.
-*/
+// Approach : DFS + recursion
+// Time Complexity : O(n) -> no.of.nodes , Space Complexity : O(h) -> height of tree
 public class FindLongZigZagPath_1372 {
     public static void main(String[] args) {
 
@@ -39,26 +27,32 @@ public class FindLongZigZagPath_1372 {
             this.val = val;
         }
     }
+
+    // Store the longest ZigZag found
     static int answer = 0;
 
     static int longestPath(TreeNode root){
-        if(root == null){
+        if(root == null){         // Empty Tree Check
             return 0;
         }
 
-        dfs(root , 0,0);
+        dfs(root , 0,0);          // Start DFS -> node =1 , left = 0 , right = 0
 
         return answer;
     }
     static void dfs(TreeNode node , int left , int right){
 
+        // if node doesn't exist stop
         if(node == null){
             return;
         }
 
+        // update the maximum answer
         answer = Math.max(answer , Math.max(left , right));
 
+        // Go LEFT
         dfs(node.left , right +1 , 0);
+        // Go RIGHT
         dfs(node.right , 0 , left +1);
 
     }
