7eeb9ede263138ef92f8cbd668dcb1397b2d7644
Alsabith <alsabith080207@gmail.com>
2026-09-23 19:07:25 +0530

    Update LowestCommonAncestor_236.java

--- BinarySearchTree_Problems/LowestCommonAncestor_236.java
+++ BinarySearchTree_Problems/LowestCommonAncestor_236.java
@@ -1,4 +1,3 @@
-package LeetCodeEx.BST;
 
 // https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/
 // Approach : Recursion -> Search both sides, if p and q are found on different sides of a node , 
