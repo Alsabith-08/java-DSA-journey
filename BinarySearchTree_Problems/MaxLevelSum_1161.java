ab80bf46131d1b1f9dc70398dda4af76fbe7d420
Alsabith <alsabith080207@gmail.com>
2026-09-25 17:54:48 +0530

    Update MaxLevelSum_1161.java

--- BinarySearchTree_Problems/MaxLevelSum_1161.java
+++ BinarySearchTree_Problems/MaxLevelSum_1161.java
@@ -1,18 +1,14 @@
-package LeetCodeEx.BST;
 
 // https://leetcode.com/problems/maximum-level-sum-of-a-binary-tree/description/
-/*     Given the root of a binary tree, the level of its root is 1,
-       the level of its children is 2, and so on.
-
-       Return the smallest level x such that the sum of all the values of nodes at level x is maximal.
- */
-
+// Approach : BFS / Level by Level
+// Time Complexity : O(n)   , Space Complexity : O(n)
 
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class MaxLevelSum_1161 {
     public static void main(String[] args) {
+           
         TreeNode root = new TreeNode(1);
 
         root.left = new TreeNode(7);
@@ -25,6 +21,7 @@ public class MaxLevelSum_1161 {
 
         System.out.println("Maximum Level is :" + result);
     }
+    // TreeNode Structure
     static class TreeNode{
         int val;
         TreeNode left;
@@ -34,29 +31,30 @@ public class MaxLevelSum_1161 {
             this.val = val;
         }
     }
-
+    // Function
     static int maxSum(TreeNode root){
-        if(root == null){
-            return 0;
+         
+        if(root == null){                              // Base Case
+            return 0; 
         }
 
-        Queue<TreeNode> queue = new LinkedList<>();
+        Queue<TreeNode> queue = new LinkedList<>();   // Create a Queue and add root
         queue.add(root);
 
-        int level = 1;
-        int answer = 0;
-        int maxSum = Integer.MIN_VALUE;
+        int level = 1;                                // maintain level -> current level 
+        int answer = 0;                               // maintain level -> store the result
+        int maxSum = Integer.MIN_VALUE;               // maintain maxSum -> this can follow the level which contain maximum sum 
 
-        while (!queue.isEmpty()){
+        while (!queue.isEmpty()){                     // BFS
             int sum =0;
             int size = queue.size();
 
             for (int i = 0; i < size; i++) {
                 TreeNode current = queue.poll();
 
-                sum += current.val;
+                sum += current.val;                   // add the current node value with sum and put it to sum.
 
-                if(current.left != null){
+                if(current.left != null){             // left and right node is not null add to queue
                     queue.add(current.left);
                 }
 
@@ -65,12 +63,12 @@ public class MaxLevelSum_1161 {
                 }
             }
 
-            if(sum > maxSum){
-                maxSum = sum;
+            if(sum > maxSum){                       // update only when the sum is greater than maxSum 
+                maxSum = sum;                       // NOTE : Don't use >= (greater than or equal) because if two level contains same sum, return the minimum level
                 answer = level;
             }
-            level++;
+            level++;                                // increase the level by 1
         }
-        return answer;
+        return answer;                              // return answer
     }
 }
